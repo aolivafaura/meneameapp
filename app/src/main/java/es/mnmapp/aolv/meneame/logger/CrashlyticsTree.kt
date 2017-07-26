b12bf0f770e0511ff8c3341f10e1e17709f2f9a3
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2017-07-26 15:08:33 +0200

    Added crashlytics logger. Added keystore for production releases

---
+++ app/src/main/java/es/mnmapp/aolv/meneame/logger/CrashlyticsTree.kt
@@ -0,0 +1,29 @@
+package es.mnmapp.aolv.meneame.logger
+
+import android.util.Log
+import com.crashlytics.android.Crashlytics
+import timber.log.Timber
+
+/**
+ * Created by antoniojoseoliva on 26/07/2017.
+ */
+
+class CrashlyticsTree : Timber.Tree() {
+
+    val CRASHLYTICS_KEY_PRIORITY = "priority"
+    val CRASHLYTICS_KEY_TAG = "tag"
+    val CRASHLYTICS_KEY_MESSAGE = "message"
+
+    override fun log(priority : Int, tag : String, message : String, t : Throwable?) {
+
+        when (priority) {
+            Log.VERBOSE, Log.DEBUG, Log.INFO -> return
+        }
+
+        Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
+        Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
+        Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
+
+        t?.let { Crashlytics.logException(t) } ?: Crashlytics.logException(Exception(message))
+    }
+}
