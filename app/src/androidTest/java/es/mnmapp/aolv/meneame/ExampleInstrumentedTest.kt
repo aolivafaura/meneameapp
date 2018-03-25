52680240d17e638156a09fc24b9eb82d47c73738
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2018-03-25 15:05:18 +0200

    Added espresso dependencies. Removed unused tests

--- app/src/androidTest/java/es/mnmapp/aolv/meneame/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/es/mnmapp/aolv/meneame/ExampleInstrumentedTest.kt
@@ -2,7 +2,6 @@ package es.mnmapp.aolv.meneame
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import android.util.Log
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,12 +11,13 @@ import org.junit.runner.RunWith
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class) class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
 
-    @Test fun testUseAppContext() {
+    @Test
+    fun testUseAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        Log.d("", appContext.packageName)
-        assertEquals("es.mnmapp.aolv.meneame.dev", appContext.packageName)
+        assertEquals("es.mnmapp.aolv.meneame.dvlp", appContext.packageName)
     }
 }
