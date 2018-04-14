91ea3668bec0c51f8a22384c6e2c245dfc3b9006
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2018-04-14 09:42:57 +0200

    Removed canary leaks

--- app/src/debug/java/es/mnmapp/aolv/meneame/MnmDebugApp.kt
+++ app/src/debug/java/es/mnmapp/aolv/meneame/MnmDebugApp.kt
@@ -1,7 +1,6 @@
 package es.mnmapp.aolv.meneame
 
 import com.facebook.stetho.Stetho
-import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by antoniojoseoliva on 20/07/2017.
@@ -10,20 +9,12 @@ import com.squareup.leakcanary.LeakCanary
 class MnmDebugApp : MnmApp() {
 
     override fun onCreate() {
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return
-        }
         super.onCreate()
 
-        initLeakCanary()
         initStetho()
     }
 
     private fun initStetho() {
         Stetho.initializeWithDefaults(this)
-    }
-
-    private fun initLeakCanary() {
-        LeakCanary.install(this)
     }
 }
