4a92c9be2c4025e1d7526d65e5b91b7e78774291
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2018-05-19 00:38:28 +0200

    Added new tests to news list viewmodel

--- app/src/test/java/es/mnmapp/aolv/meneame/ui/view/newslist/fragment/NewsListViewModelTest.kt
+++ app/src/test/java/es/mnmapp/aolv/meneame/ui/view/newslist/fragment/NewsListViewModelTest.kt
@@ -2,10 +2,13 @@ package es.mnmapp.aolv.meneame.ui.view.newslist.fragment
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.argumentCaptor
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
+import es.mnmapp.aolv.domain.entity.New
 import es.mnmapp.aolv.domain.usecase.GetPopularNews
 import es.mnmapp.aolv.meneame.ui.view.newslist.NewsListViewModel
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -36,9 +39,34 @@ class NewsListViewModelTest {
     }
 
     @Test
-    fun `When news are loaded for first time, then popular news are retrieved`() {
+    fun `Given user on news list screen, when news are fetched, then popular news use case is called`() {
         newsListViewModel.fetchNews()
-
         verify(getPopularNews, times(1)).execute(any(), any(), any(), any())
+    }
+
+    @Test
+    fun `Given news being fetched, when response is being waited, then view state is refreshing`() {
+        newsListViewModel.fetchNews()
+        assertEquals(NewsListViewModel.ViewState.Refreshing, newsListViewModel.state.value)
+    }
+
+    @Test
+    fun `Given news being fetched, when successful response, then view state is idle`() {
+        val successCaptor = argumentCaptor<(List<New>) -> Unit>()
+
+        newsListViewModel.fetchNews()
+        verify(getPopularNews, times(1)).execute(any(), successCaptor.capture(), any(), any())
+        successCaptor.firstValue.invoke(emptyList())
+        assertEquals(NewsListViewModel.ViewState.Idle, newsListViewModel.state.value)
+    }
+
+    @Test
+    fun `Given news being fetched, when error response, then view state is idle`() {
+        val errorCaptor = argumentCaptor<(Throwable) -> Unit>()
+
+        newsListViewModel.fetchNews()
+        verify(getPopularNews, times(1)).execute(any(), any(), errorCaptor.capture(), any())
+        errorCaptor.firstValue.invoke(Throwable())
+        assertEquals(NewsListViewModel.ViewState.Idle, newsListViewModel.state.value)
     }
 }
