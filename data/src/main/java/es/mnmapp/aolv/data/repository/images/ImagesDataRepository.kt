5cabfd5114c2136902817c1f7be0d2c9dc287c36
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2018-06-09 01:20:53 +0200

    Re-idented files on data module

--- data/src/main/java/es/mnmapp/aolv/data/repository/images/ImagesDataRepository.kt
+++ data/src/main/java/es/mnmapp/aolv/data/repository/images/ImagesDataRepository.kt
@@ -1,3 +1,19 @@
+/*
+ *     Copyright 2018 @ https://github.com/aolivafaura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package es.mnmapp.aolv.data.repository.images
 
 import es.mnmapp.aolv.data.EndpointUrls
@@ -11,29 +27,28 @@ import es.mnmapp.aolv.domain.repository.ScreenDensity
 import io.reactivex.Single
 import io.reactivex.internal.operators.single.SingleJust
 
-
 class ImagesDataRepository(
-        private val placeholdersCloudDataSource: PlaceholdersCloudDataSource,
-        private val placeholdersLocalDataSource: PlaceholdersLocalDataSource
+    private val placeholdersCloudDataSource: PlaceholdersCloudDataSource,
+    private val placeholdersLocalDataSource: PlaceholdersLocalDataSource
 ) : ImagesRepository {
 
     override fun getPlaceholders(density: ScreenDensity): Single<List<Placeholder>> {
         fun handleLocalResponse(list: List<PlaceholderEntity>) =
-                if (list.isNotEmpty()) {
-                    SingleJust(list)
-                } else {
-                    placeholdersCloudDataSource.getPlaceholders(density)
-                            .doOnSuccess { insertOnDatabase(it) }
-                            .onErrorReturn { list }
-                }
+            if (list.isNotEmpty()) {
+                SingleJust(list)
+            } else {
+                placeholdersCloudDataSource.getPlaceholders(density)
+                    .doOnSuccess { insertOnDatabase(it) }
+                    .onErrorReturn { list }
+            }
 
         return placeholdersLocalDataSource.getAll()
-                .flatMap { handleLocalResponse(it) }
-                .map { it.map { mapToPlaceholder(it) } }
+            .flatMap { handleLocalResponse(it) }
+            .map { it.map { mapToPlaceholder(it) } }
     }
 
     override fun getLogosForSources(vararg source: String): Single<Map<String, String>> =
-            Single.just(source.map { it to "${EndpointUrls.logoApiUrl}$it" }.toMap())
+        Single.just(source.map { it to "${EndpointUrls.logoApiUrl}$it" }.toMap())
 
     private fun insertOnDatabase(list: List<PlaceholderEntity>) {
         placeholdersLocalDataSource.insertAll(*list.toTypedArray())
--- data/src/main/java/es/mnmapp/aolv/data/repository/images/datasource/local/PlaceholderRoom.kt
+++ data/src/main/java/es/mnmapp/aolv/data/repository/images/datasource/local/PlaceholderRoom.kt
@@ -1,3 +1,19 @@
+/*
+ *     Copyright 2018 @ https://github.com/aolivafaura
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package es.mnmapp.aolv.data.repository.images.datasource.local
 
 import android.arch.persistence.room.ColumnInfo
@@ -6,18 +22,19 @@ import android.arch.persistence.room.PrimaryKey
 import es.mnmapp.aolv.data.entity.PlaceholderEntity
 
 @Entity(tableName = "placeholder")
-data class PlaceholderRoom(@PrimaryKey @ColumnInfo(name = "category") val category: String,
-                           @ColumnInfo(name = "url") val url: String
+data class PlaceholderRoom(
+    @PrimaryKey @ColumnInfo(name = "category") val category: String,
+    @ColumnInfo(name = "url") val url: String
 )
 
-fun mapToPlaceholderEntity(placeholderRoom: PlaceholderRoom) =
-        PlaceholderEntity(
-                placeholderRoom.category,
-                placeholderRoom.url
-        )
+fun mapToPlaceholderEntity(placeholderRoom: PlaceholderRoom): PlaceholderEntity =
+    PlaceholderEntity(
+        placeholderRoom.category,
+        placeholderRoom.url
+    )
 
-fun mapFromPlaceholderEntity(placeholderEntity: PlaceholderEntity) =
-        PlaceholderRoom(
-                placeholderEntity.category,
-                placeholderEntity.url
-        )
+fun mapFromPlaceholderEntity(placeholderEntity: PlaceholderEntity): PlaceholderRoom =
+    PlaceholderRoom(
+        placeholderEntity.category,
+        placeholderEntity.url
+    )
