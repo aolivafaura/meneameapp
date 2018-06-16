d79d6d1abf83303aa72ef080b789dfdadae62b3e
Antonio José Oliva Faura <a.oliva.faura@gmail.com>
2018-06-16 12:40:28 +0200

    Lgr code refactored

--- logger/src/main/java/com/antoniooliva/logger/Lgr.kt
+++ logger/src/main/java/com/antoniooliva/logger/Lgr.kt
@@ -23,44 +23,30 @@ import android.app.Application
 import android.content.Context
 import android.content.ContextWrapper
 import android.os.Bundle
-import android.util.Log
-import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
-import com.google.firebase.analytics.FirebaseAnalytics
-import io.fabric.sdk.android.Fabric
-import timber.log.Timber
 
 class Lgr private constructor(context: Context) : ContextWrapper(context) {
 
     fun d(message: String?) {
-        Timber.d(message)
+        ServiceLocator.provideTimber().d(message)
     }
 
     fun w(message: String?) {
-        Timber.w(message)
+        ServiceLocator.provideTimber().w(message)
     }
 
     fun i(message: String?) {
-        Timber.i(message)
+        ServiceLocator.provideTimber().i(message)
     }
 
     fun v(message: String?) {
-        Timber.v(message)
+        ServiceLocator.provideTimber().v(message)
     }
 
     fun e(message: String?) {
-        Timber.e(message)
+        ServiceLocator.provideTimber().e(message)
     }
 
     fun logEvent(name: String, params: Map<String, String>?) {
-        if (BuildConfig.DEBUG) logDebugEvent(name, params) else logReleaseEvent(name, params)
-    }
-
-    private fun logDebugEvent(name: String, params: Map<String, String>?) {
-        Timber.i("AnalyticsLogger EVENT LOGGED: $name, $params")
-    }
-
-    private fun logReleaseEvent(name: String, params: Map<String, String>?) {
         val bundle = Bundle().apply {
             params?.let {
                 for (entry in it.entries) {
@@ -69,40 +55,11 @@ class Lgr private constructor(context: Context) : ContextWrapper(context) {
             }
         }
 
-        FirebaseAnalytics.getInstance(baseContext).logEvent(name, bundle)
+        ServiceLocator.provideFirebaseAnalytics(baseContext).logEvent(name, bundle)
     }
 
     fun setCurrentScreen(activity: Activity, screen: String) {
-        if (BuildConfig.DEBUG) setDebugCurrentScreen(screen) else setReleaseCurrentScreen(activity, screen)
-    }
-
-    private fun setDebugCurrentScreen(screen: String) {
-        Timber.i("AnalyticsLogger SCREEN SET: $screen")
-    }
-
-    private fun setReleaseCurrentScreen(activity: Activity, screen: String) {
-        FirebaseAnalytics.getInstance(activity).setCurrentScreen(activity, screen, null)
-    }
-
-    class CrashlyticsTree : Timber.Tree() {
-
-        override fun log(priority: Int, tag: String, message: String, t: Throwable?) {
-            when (priority) {
-                Log.VERBOSE, Log.DEBUG, Log.INFO -> return
-            }
-
-            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority)
-            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag)
-            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message)
-
-            t?.let { Crashlytics.logException(t) } ?: Crashlytics.logException(Exception(message))
-        }
-
-        companion object {
-            private const val CRASHLYTICS_KEY_PRIORITY = "priority"
-            private const val CRASHLYTICS_KEY_TAG = "tag"
-            private const val CRASHLYTICS_KEY_MESSAGE = "message"
-        }
+        ServiceLocator.provideFirebaseAnalytics(activity).setCurrentScreen(activity, screen, null)
     }
 
     companion object {
@@ -111,18 +68,17 @@ class Lgr private constructor(context: Context) : ContextWrapper(context) {
         @Synchronized
         fun initialize(application: Application) {
             if (selfInstance == null) {
-                Timber.plant(if (BuildConfig.DEBUG) Timber.DebugTree() else CrashlyticsTree())
-
-                val core = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
-                Fabric.with(application, Crashlytics.Builder().core(core).build())
-
+                Starter.start(application)
                 selfInstance = Lgr(application.applicationContext)
+            } else {
+                throw IllegalStateException("Lgr already initialized. Cannot initialize it twice")
             }
         }
 
         @Synchronized
         fun get(): Lgr =
-                selfInstance?.let { it }
-                        ?: throw IllegalStateException("Lgr must be initialized first")
+            selfInstance?.let {
+                it
+            } ?: throw IllegalStateException("Lgr must be initialized first")
     }
 }
